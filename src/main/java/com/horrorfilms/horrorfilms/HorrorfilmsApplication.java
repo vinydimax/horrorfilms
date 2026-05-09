a9c1980f5e0f803c735db76c1737663eca045cbf
Vinicius Sanches <kurosawa93@gmail.com>
2026-05-09 02:41:17 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/horrorfilms/horrorfilms/HorrorfilmsApplication.java
@@ -0,0 +1,13 @@
+package com.horrorfilms.horrorfilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorrorfilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorrorfilmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horrorfilms/horrorfilms/HorrorfilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horrorfilms.horrorfilms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorrorfilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
